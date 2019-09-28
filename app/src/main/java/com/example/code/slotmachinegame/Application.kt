8256cc6a6c3ee428d3506f9749b60480272924e1
Vlad <vladleschenko777@gmail.com>
2019-09-28 19:38:20 +0300

    init app

---
+++ app/src/main/java/com/example/code/slotmachinegame/Application.kt
@@ -0,0 +1,13 @@
+package com.example.code.slotmachinegame
+
+import androidx.multidex.MultiDexApplication
+
+class Application : MultiDexApplication() {
+
+
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+
+}
